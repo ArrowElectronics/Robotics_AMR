be9476767e696f2a1d9e7a5e42c7eb62a285a645
Vishal Raval <Vishal.Raval@einfochips.com>
2023-03-28 13:43:05 +0530

    add run.sh file for galactic docker modify tag latest to release humble's run.sh file
    Signed-off-by: Vishal Raval <Vishal.Raval@einfochips.com>

---
+++ docker/galactic/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+docker stop ros2-galactic-focal > /dev/null
+docker rm -f ros2-galactic-focal > /dev/null
+mkdir -p adi-dev
+docker run \
+	--privileged \
+	-itd \
+	--init \
+	--rm \
+	--network host \
+	-v /dev:/dev \
+	-v /tmp/.x11-unix:/tmp/.x11-unix \
+	-e DISPLAY \
+	-v $(pwd)/adi-dev:/home/admin/adi-dev:rw \
+	-u $(id -u) \
+	--name ros2-galactic-focal \
+	ros2-galactic-focal:release
